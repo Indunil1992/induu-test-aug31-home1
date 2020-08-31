1c79c36beec56cd060ddc565384dfa2242662c56
Indunil1992 <52061174+Indunil1992@users.noreply.github.com>
2020-08-31 15:41:54 +0530

    Update induu-test-aug31-home1.js

--- induu-test-aug31-home1.js
+++ induu-test-aug31-home1.js
@@ -4,7 +4,7 @@ const s3 = new AWS.S3();
 exports.handler = async (event) => {
     try {
         let data = await s3.listObjects({
-            Bucket: "induuuu",
+            Bucket: "as2-test-lahiru",
             MaxKeys: 10
         }).promise();
 
@@ -12,5 +12,5 @@ exports.handler = async (event) => {
         // error handling goes here
     };
 
-    return { "message": "Successfully executed-1" };
+    return { "message": "Successfully executed-2" };
 };
