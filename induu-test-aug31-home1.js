fd2e2dd5d4f485abe1b47df29483c031b16c5b0a
Indunil1992 <indunil@adroitlogic.com>
2020-08-31 16:11:46 +0530

    persisit-10

--- induu-test-aug31-home1.js
+++ induu-test-aug31-home1.js
@@ -12,5 +12,5 @@ exports.handler = async (event) => {
         // error handling goes here
     };
 
-    return { "message": "Successfully executed-3" };
+    return { "message": "Successfully executed-4" };
 };
